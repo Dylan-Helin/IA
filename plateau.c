7cf4f8bb7782f6e7d303fc4f586adeaab5d66568
Dylan-Helin <Dylan-Helin@users.noreply.github.com>
2020-11-19 10:57:23 +0100

    Fonctions de calcul des coups jouables

--- plateau.c
+++ plateau.c
@@ -4,13 +4,17 @@
 
 #include "plateau.h"
 
-void affichage (Plateau p){
+void affichage (Plateau p,CoupsJouables tab){
     printf("---------------------------------\n");
     for (int i = 0;i<8;i++){
         for (int j = 0;j<8;j++){
             printf("|");
             if(p[j][i]==0){
-                printf("   ");
+                if(appartient(j,i,tab)==1){
+                    printf(" 0 ");
+                }else {
+                    printf("   ");
+                }
             }
             if(p[j][i]==1){
                 printf(" b ");
@@ -22,4 +26,34 @@ void affichage (Plateau p){
         printf("|\n");
         printf("---------------------------------\n");
     }
+}
+
+void initialiserLesCoups(CoupsJouables tab,Plateau p,int couleur){
+    for(int i = 0;i<64;i++){
+        tab[i].x=-1;
+        tab[i].y=-1;
+    }
+    int k=0;
+    for (int i = 0;i<8;i++) {
+        for (int j = 0; j < 8; j++) {
+            if(p[j][i]==0){
+                if((i>0 && p[j][i-1]==couleur*-1) || (i<7 && p[j][i+1]==couleur*-1) || (j>0 && p[j-1][i]==couleur*-1) || (j<7 && p[+1][i]==couleur*-1)){
+                    tab[k].x=j;
+                    tab[k].y=i;
+                }
+            }
+        }
+    }
+}
+
+int appartient (int x,int y, CoupsJouables tab){
+    for (int i = 0;i<64;i++){
+        if(tab[i].x==-1 && tab[i].y==-1){
+            break;
+        }
+        if(tab[i].x==x && tab[i].y==y){
+            return 1;
+        }
+    }
+    return 0;
 }
--- plateau.h
+++ plateau.h
@@ -5,8 +5,16 @@
 #ifndef IA_PLATEAU_H
 #define IA_PLATEAU_H
 #include <stdio.h>
+
 typedef int Plateau[8][8];
+typedef struct{
+    int x;
+    int y;
+}Coord;
+typedef Coord CoupsJouables[64];
 
 
-void affichage (Plateau p); //affiche le plateau de jeu
+void affichage (Plateau p,CoupsJouables tab); //affiche le plateau de jeu
+int appartient (int x,int y, CoupsJouables tab); //renvoi 1 si les coordonnées données sont jouables 0 sinon
+void initialiserLesCoups(CoupsJouables tab,Plateau p,int couleur); //liste tous les coups jouables pour la couleur actuelle
 #endif //IA_PLATEAU_H
