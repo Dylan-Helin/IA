d6224c24c2b5d3829e546d5bb94feb358eb8eae4
Angele-Guery <Angele-Guery@users.noreply.github.com>
2020-11-19 10:02:26 +0100

    struct joueur créé

--- head.h
+++ head.h
@@ -5,7 +5,14 @@
 #ifndef IA_HEAD_H
 #define IA_HEAD_H
 
+#include <stdbool.h>
 
+typedef struct Joueur {
+    int numero; //joueur 1 ou 2
+    int couleur; //1 pour blanc, 2 pour noir (aléatoire en début de partie)
+    bool IA; //true si c'est une IA (le jueur choisit ça dans les options ou en début de jeux)
+
+};
 
 
 
