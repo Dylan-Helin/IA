3fdeb58010fbc5f1a0540df5b488b94e1ff323a0
Angele-Guery <Angele-Guery@users.noreply.github.com>
2020-12-03 10:45:52 +0100

    creation et destruction d'enfants

--- IA.c
+++ IA.c
@@ -5,3 +5,12 @@ HELIN Dylan
 
 #include "head.h"
 
+Enfants* creationEnfant(){
+    Enfants* e;
+    e=(Enfants*) malloc(sizeof(Enfants) );
+    return e;
+}
+
+void destructionEnfant(Enfants e){
+    free(e);
+}
